2af2561d784a8cac0d785aacc3a04c364c0617ae
Eduardc77 <36419167+eduardc77@users.noreply.github.com>
2025-09-18 16:49:20 +0300

    Add files via upload

---
+++ Movies/Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by User on 9/18/25.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
